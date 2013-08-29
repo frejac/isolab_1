7d25fb8bb8ad8835fdd97c7bfaf6b9849a6152b8
wenneborg <erik.wenneborg@isotop.se>
2013-08-29 22:10:30 +0200

    some js

---
+++ Lists.js
@@ -0,0 +1,635 @@
+﻿var Lists =
+[
+	{ "name": "Huvudstäder i Afrika",
+		"items":
+[
+      {
+      	"name": "Alger",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Luanda",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Porto-Novo",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Gaborone",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Ouagadougou",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Bujumbura",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Bangui",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Djibouti",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Kairo",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Malabo",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Yamoussoukro",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Asmara",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Addis Ababa",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Libreville",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Banjul",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Accra",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Conakry",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Bissau",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Yaoundé",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Praia",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Nairobi",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Moroni",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Brazzaville",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Kinshasa",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Maseru",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Monrovia",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Tripoli",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Antananarivo",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Lilongwe",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Bamako",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Rabat",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Nouakchott",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Port Louis",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Dzaoudzi",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Maputo",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Windhoek",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Niamey",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Abuja",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Saint-Denis",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Kigali",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Jamestown",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "São Tomé",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Dakar",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Victoria",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Freetown",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Mogadishu",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Khartoum",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Mbabane",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Pretoria",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Dodoma",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "N'Djamena",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Lomé",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Tunis",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Kampala",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Al-Ayun",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Lusaka",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      },
+      {
+      	"name": "Harare",
+      	"metadata": [
+          {
+
+          },
+          {
+
+          }
+        ]
+      }
+    ]
+	}
+];
